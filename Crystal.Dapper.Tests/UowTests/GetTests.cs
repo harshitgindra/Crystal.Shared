eb15ac65b46021991b9bd6d504e937d0b59fcdc2
Harshit Gindra <harshitgindra@gmail.com>
2021-03-11 18:18:29 -0500

    Fixng project dependencies with nuget pkgs

--- Crystal.Dapper.Tests/UowTests/GetTests.cs
+++ Crystal.Dapper.Tests/UowTests/GetTests.cs
@@ -1,8 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using MicroOrm.Dapper.Repositories.SqlGenerator;
 
 namespace Crystal.Dapper.Tests.UowTests
 {
