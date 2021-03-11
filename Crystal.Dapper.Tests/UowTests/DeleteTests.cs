2ea2ca3e638b71f1f7427396ecb0c07e6c7e0f8b
Harshit Gindra <harshitgindra@gmail.com>
2021-03-11 18:14:35 -0500

    Delete tests

---
+++ Crystal.Dapper.Tests/UowTests/DeleteTests.cs
@@ -0,0 +1,238 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crystal.Dapper.Tests.UowTests
+{
+    [TestFixture]
+    public class DeleteTests : BaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            this.Init();
+            _sampleProduct = new Product()
+            {
+                Value = 70,
+                Name = "Sample 1"
+            };
+            _sampleProducts = new List<Product>()
+            {
+                new Product()
+                {
+                    Value = 70,
+                    Name = "Sample 1"
+                },
+                new Product()
+                {
+                    Value = 70,
+                    Name = "Sample 2"
+                },
+            };
+        }
+
+        private Product _sampleProduct;
+        private List<Product> _sampleProducts;
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecord()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            //***
+            //*** When delete method is called
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(_sampleProduct);
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be deleted
+            //***
+            Assert.IsNull(product);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecordTransactionCommit()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete method is called with commit
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(_sampleProduct);
+            await UowRepository.CommitAsync();
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be deleted
+            //***
+            Assert.IsNull(product);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecordTransactionRollback()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete method is called with commit
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(_sampleProduct);
+            await UowRepository.RollbackAsync();
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be not deleted
+            //***
+            Assert.IsNotNull(product);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecordsExpression()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            //***
+            //*** When delete method is called with an expression
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(x => x.ProductId == _sampleProduct.ProductId);
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be deleted
+            //***
+            Assert.IsNull(product);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecordsExpressionTransactionCommit()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete method is called with an expression and transaction commit
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(x => x.ProductId == _sampleProduct.ProductId);
+            await UowRepository.CommitAsync();
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be deleted
+            //***
+            Assert.IsNull(product);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteRecordsExpressionTransactionRollback()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProduct);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete method is called with an expression and transaction rollback
+            //***
+            await UowRepository.Repository<Product>().DeleteAsync(x => x.ProductId == _sampleProduct.ProductId);
+            await UowRepository.RollbackAsync();
+            var product = await UowRepository.Repository<Product>().FindAsync(_sampleProduct.ProductId);
+            //***
+            //*** Then: record should be not deleted
+            //***
+            Assert.IsNotNull(product);
+        }
+
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteAllRecords()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProducts);
+            //***
+            //*** When delete all method is called
+            //***
+            await UowRepository.Repository<Product>().DeleteAllAsync();
+            var product = await UowRepository.Repository<Product>().GetAsync();
+            //***
+            //*** Then: all record should be deleted
+            //***
+            Assert.AreEqual(0, product.Count);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteAllRecordsTransactionCommit()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProducts);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete all method is called with transaction commit
+            //***
+            await UowRepository.Repository<Product>().DeleteAllAsync();
+            await UowRepository.CommitAsync();
+            var product = await UowRepository.Repository<Product>().GetAsync();
+            //***
+            //*** Then: all record should be deleted
+            //***
+            Assert.AreEqual(0, product.Count);
+        }
+
+        [Test]
+        [Category("Delete")]
+        [Category("Dapper")]
+        public async Task DeleteAllRecordsTransactionRollback()
+        {
+            //***
+            //*** Given: Delete a record in the database
+            //***
+            await UowRepository.Repository<Product>().InsertAsync(_sampleProducts);
+            await UowRepository.BeginTransactionAsync();
+            //***
+            //*** When delete all method is called with transaction rollback
+            //***
+            await UowRepository.Repository<Product>().DeleteAllAsync();
+            await UowRepository.CommitAsync();
+            var product = await UowRepository.Repository<Product>().GetAsync();
+            //***
+            //*** Then: no record should be deleted
+            //***
+            Assert.AreEqual(_sampleProducts.Count, product.Count);
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+            UowRepository.Dispose();
+        }
+    }
+}
